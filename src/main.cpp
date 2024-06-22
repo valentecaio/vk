ad29c212785ea65974810a1a27b2fc0677e1d0d2
valentecaio <cvalente.aio@gmail.com>
2024-06-22 15:55:06 -0300

    indentation

--- src/main.cpp
+++ src/main.cpp
@@ -6,49 +6,45 @@
 
 using namespace vk;
 
-// application class //
 
 class Application {
-public:
-  void run() {
-    initWindow();
-    kilauea.init(window);
-    mainLoop();
-    cleanup();
-  }
-
-private:
-  GLFWwindow* window;  // window handle
-  Kilauea kilauea;     // vulkan handle
-
-  void initWindow() {
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // disable openGL context
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);   // disable resizing
-
-    // 4th argument is monitor (fullscreen), 5th is share (only for openGL)
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    // int c;
-    // auto monitors = glfwGetMonitors(&c);
-    // window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", monitors[0], nullptr);
-  }
-
-  void mainLoop() {
-    while (!glfwWindowShouldClose(window)) {
-      glfwPollEvents();
-      kilauea.drawFrame();
+  public:
+    void run() {
+      initWindow();
+      kilauea.init(window);
+      mainLoop();
+      cleanup();
     }
 
-    kilauea.waitIdle();
-  }
+  private:
+    GLFWwindow* window;  // window handle
+    Kilauea kilauea;     // vulkan handle
 
-  void cleanup() {
-    kilauea.cleanup();
+    void initWindow() {
+      glfwInit();
+      glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // disable openGL context
+      glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);   // disable resizing
 
-    // glfw
-    glfwDestroyWindow(window);
-    glfwTerminate();
-  }
+      // 4th argument is monitor (fullscreen), 5th is share (only for openGL)
+      window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void mainLoop() {
+      while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        kilauea.drawFrame();
+      }
+
+      kilauea.waitIdle();
+    }
+
+    void cleanup() {
+      kilauea.cleanup();
+
+      // glfw
+      glfwDestroyWindow(window);
+      glfwTerminate();
+    }
 };
 
 int main() {
